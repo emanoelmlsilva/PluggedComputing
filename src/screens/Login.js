06d46cfd7ae4d0eecc2ed1e207f3839ce0d8aa04
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-11-16 01:49:32 -0300

    added component src

---
+++ src/screens/Login.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'React'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
+
+
+class Login extends Component{
+    render(){
+        return(
+            <View Style={styles.container}>
+            </View>
+        )
+    }
+}
+
+
+const styles = {
+ container:{
+    flex: 1,
+    alignItem: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffff',
+ }       
+}
