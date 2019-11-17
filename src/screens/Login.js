2c90d97b5825dd65143e84334acd4cfeef4214f8
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-11-17 12:03:46 -0300

    create layout login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,22 +1,119 @@
-import React, {Component} from 'React'
-import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
+import React, { Component } from 'react';
+import { View,
+         Text,
+         StyleSheet,
+         TextInput,
+         TouchableOpacity,
+         Button,         
+         } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+import PasswordInputText from 'react-native-hide-show-password-input';
 
-class Login extends Component{
+export default class Login extends Component{
+
+    state = {
+        email: '',
+        passworld: '',
+    }
+
     render(){
         return(
-            <View Style={styles.container}>
+            <View style={styles.container}>
+             <Text style={styles.Text}>Login</Text>             
+            <View style={styles.retanguleShape}>
+                
+                <View style={styles.containerComponent}>
+                    <Icon name="envelope" size={32} />
+                    <TextInput   
+                        style={styles.input}                    
+                        placeholder="Email"
+                        onChangeText={(email) => this.setState({email})}
+                        value={this.state.email}
+                    />    
+                </View>   
+                
+                <View style={styles.separator}></View>
+                
+                <View style={styles.containerComponent}>
+                    <Icon name="lock" size={32} />
+                    <TextInput              
+                        secureTextEntry={true}
+                        style={styles.input}
+                        placeholder="Senha"
+                        onChangeText={(passworld) => this.setState({passworld})}
+                        value={this.state.passworld}
+                    />                              
+                </View>
+            </View>
+            <TouchableOpacity style={styles.containerButton}>            
+                <Text style={styles.textButton}>Entrar</Text>
+            </TouchableOpacity>      
             </View>
         )
     }
 }
 
 
-const styles = {
- container:{
-    flex: 1,
-    alignItem: 'center',
+const styles = StyleSheet.create({
+ container:{       
+    flexDirection: 'column',
+    alignItems:'center',     
+    width: '100%',
+    height: '100%', 
+    backgroundColor: '#1280e5',   
+
+ },
+ retanguleShape:{      
+    borderRadius: 8,
+    backgroundColor: '#FFFFFF',
+    width: '80%',
+    height: 200, 
+    
+
+ },
+ Text:{     
+    color: '#FFFFFF',
+    fontSize: 54,       
+    marginBottom: 40,    
+    textAlign: 'center',
+    marginTop: '30%',
+ },
+ input:{        
+        marginLeft: 20,        
+        width: '90%',        
+        height: 40,
+        color: '#000000'  
+                          
+},
+separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginLeft: 20,
+    marginRight: 20,
+},
+containerButton:{    
+    borderRadius: 5,
     justifyContent: 'center',
-    backgroundColor: '#ffff',
- }       
+    alignItems: 'center',
+    backgroundColor: '#1280e5',   
+    height: 50,
+    width: '60%',     
+    marginTop: 20,
+    borderColor: '#000000',
+    borderWidth: 0.5,
+    
+
+},
+textButton: {    
+    fontSize: 18,
+    textTransform: 'uppercase',
+    color: '#FFFFFF',
+},
+containerComponent: {    
+    flexDirection: 'row',
+    marginTop: 40,
+    marginLeft: 20,                                 
 }
+})
